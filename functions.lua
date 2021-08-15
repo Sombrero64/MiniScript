7b1751e88c415ad2f634a8af1b7adb725647f013
Daniel <DanieliToast@gmail.com>
2021-08-15 15:58:04 -0500

    Add files via upload

---
+++ functions.lua
@@ -0,0 +1,61 @@
+local functions = {}
+
+-- rounds the number
+function functions.round(number)
+	return number >= 0 and math.floor(number + 0.5) or math.ceil(number - 0.5)
+end
+
+-- checks if a number is an integer
+function functions.isInteger(number)
+	return math.floor(number) == number
+end
+
+-- random float number
+function functions.randomFloat(min, max)
+	return min + math.random() * (max - min);
+end
+
+-- checks if an file exists
+function functions.checkFile(file)
+	local test = io.open(file, "rb")
+	if test then test:close() end
+	return test ~= nil
+end
+
+-- reads a file and returns a table with each line as a string
+function functions.readFile(file)
+	if not functions.checkFile(file) then return {} end
+	local lines = {}
+	for line in io.lines(file) do
+		lines[#lines + 1] = line
+	end
+	return lines
+end
+
+-- ask the user for input and returns it
+function functions.askUser(question)
+	local question = quesiton or ""
+	return io.read()
+end
+
+-- run code only if in interpreter
+function functions.interpreterCommand(args, code)
+	if inInterpreter then
+		code()
+	else
+		print("This command can only be used in the interpreter!")
+	end
+end
+
+-- print the items from a table
+function functions.printTable(list, showIndexes)
+	for index, item in pairs(list) do
+		local args = {item}
+		if showIndexes == nil or showIndexes then
+			table.insert(args, 1, index)
+		end
+		print(table.unpack(args))
+	end
+end
+
+return functions
---
+++ strings.lua
@@ -0,0 +1,57 @@
+local strings = {}
+
+strings.help = {
+	["set"] = {"set name value", "sets the value of a variable"},
+	["delete"] = {"delete variable/list/procedure name", "deletes a variable, list, or procedure"},
+	["list"] = {"list name ...", "sets the items in a list"},
+	["insert"] = {"insert list value ~ index", "adds a new item at the end or a specified index in a list"},
+	["remove"] = {"remove list index", "removes an item from a list"},
+	["replace"] = {"replace list index value", "sets the value of an item from a list"},
+	["item"] = {"item output list index", "gets the value of an item in a list"},
+	["find"] = {"find output list value", "gets the index of the first item that equals to the requested value"},
+	["size"] = {"size output list", "gets the amount of items in a list"},
+	["add"] = {"add output ...", "returns the sum of all inputs"},
+	["subtract"] = {"subtract output ...", "returns the difference of all inputs"},
+	["multiply"] = {"multiply output ...", "returns the product of all inputs"},
+	["divide"] = {"divide output ...", "returns the quotient of all inputs"},
+	["remainer"] = {"remainer output ...", "returns the remainer of all inputs"},
+	["round"] = {"round output number ~ force", "rounds the number depending on the value, or only up or down"},
+	["random"] = {"random output min max", "generates a random number bewteen two numbers"},
+	["absolute"] = {"absolute output number", "returns the absolute value of the number"},
+	["join"] = {"join output ...", "compines all inputs into a longer string"},
+	["length"] = {"length output string", "returns the length of a string"},
+	["sub"] = {"sub output string start/index ~ end", "gets a section of characters or a character from a string"},
+	["not"] = {"not output value", "returns the opposite boolean value"},
+	["any"] = {"any output ...", "returns true if any of the boolean inputs is true"},
+	["all"] = {"any output ...", "returns true if all boolean inputs are true"},
+	["equal"] = {"equal output ...", "returns true if all values are equal"},
+	["order"] = {"order output ...", "returns true if the values are correctly ordered from greatest to smallest"},
+	["goto"] = {"goto line", "sets the line number"},
+	["step"] = {"step count", "changes the line number"},
+	["goif"] = {"goif truth if else", "changes the line number to either IF or ELSE depending the boolean value"},
+	["check"] = {"check output if else", "returns either IF or ELSE depending on the boolean value"},
+	["print"] = {"print ...", "displays strings to the console"},
+	["ask"] = {"ask output", "asks the user then returns the anwser"},
+	["about"] = {"about", "display information on miniscript"},
+	["help"] = {"help ~ command", "display information on a command or list every available command"},
+	["run"] = {"run path", "runs a new program, stops current script"},
+	["reset"] = {"reset", "removes all variables, lists, and procedures"},
+	["parse"] = {"parse line", "parses and runs the string as code"},
+	["define"] = {"define name ~ inputs", "defines a new proceure, storing code and inputs"},
+	["call"] = {"call name ...", "run a proceure"},
+	["if"] = {"if truth if_line ~ else_line", "runs either IF or ELSE depending on the boolean value"},
+}
+
+strings.about = {
+	"MiniScript Version 2.1",
+	"MiniScript is a basic and small programming language.",
+	"Licensed under the MIT license, created by Daniel Lawson."
+}
+
+strings.start = {
+	"MiniScript Version 2.1",
+	"Use the run command to run a script.",
+	"Use the help command for more info on each command."
+}
+
+return strings
