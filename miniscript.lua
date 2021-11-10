fef01caa24098b45e59340e7edfe11afcca62222
Daniel <DanieliToast@gmail.com>
2021-11-09 19:03:02 -0600

    Update miniscript.lua

--- miniscript.lua
+++ miniscript.lua
@@ -1,24 +1,12 @@
 local data = require("modules/data")
-local functions = require("modules/functions")
-local parser = require("modules/parser")
+local run = data.runLine
 
-local runCommand = data.runCommand
-local introText = [[MiniScript Interpreter 2.0
+print([[MiniScript Interpreter 2.1
 Type "run" with path to run a program
-Type "help" to list all commands
-Type "about" for info on MiniScript]]
-
-print(introText)
+Type "about" for info on MiniScript]])
 
 while true do
 	local input = io.read() or ""
-	local text = parser.cleanUpString(input)
-	if string.gsub(text, "%s+", "") ~= "" then
-		local tokens = parser.tokenizer(text)
-		local set = parser.generate(tokens)
-		local output = data.runCommand(set)
-		if output ~= nil then
-			print(tostring(output))
-		end
-	end
+	local output = run(input)
+	print(tostring(output))
 end
